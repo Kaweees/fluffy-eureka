dbf32b429ecc4096be8cf3b33b1c4dde5a3f3902
Miguel Villa Floran <49963287+Kaweees@users.noreply.github.com>
2022-08-26 17:05:51 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,37 @@
 #!/usr/bin/env bash
 
 #################
-# Let's install some ROS prereqs.
+# Let's install some ROS 2 (Foxy) prereqs.
 # Are you ready?
 ################
 
+###
+# Let's set up a locale which supports UTF-8 encoding
+###
+
+locale  # check for UTF-8
+
+sudo apt update && sudo apt install locales
+sudo locale-gen en_US en_US.UTF-8
+sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
+export LANG=en_US.UTF-8
+
+locale  # verify settings
+
+###
+# Enable the Ubuntu Universe repository
+###
+
+sudo apt install software-properties-common
+sudo add-apt-repository universe
+
+###
+# Add the ROS 2 apt repositories
+###
+
+sudo apt update && sudo apt install curl gnupg2 lsb-release
+sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key  -o /usr/share/keyrings/ros-archive-keyring.gpg
+
+###
+# Add the ROS 2 apt repositories to your sources list.
+###
