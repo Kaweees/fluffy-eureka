b7cb3bbf63c4dbb52c9989dd7ce31216c4287729
Miguel Villa Floran <49963287+Kaweees@users.noreply.github.com>
2022-08-26 17:36:04 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -26,12 +26,34 @@ sudo apt install software-properties-common
 sudo add-apt-repository universe
 
 ###
-# Add the ROS 2 apt repositories
+# Add the ROS 2 (Foxy) apt repositories
 ###
 
 sudo apt update && sudo apt install curl gnupg2 lsb-release
 sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key  -o /usr/share/keyrings/ros-archive-keyring.gpg
 
 ###
-# Add the ROS 2 apt repositories to your sources list.
+# Add the ROS 2 (Foxy) apt repositories to your system.
 ###
+
+sudo apt update && sudo apt install curl gnupg2 lsb-release
+sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key  -o /usr/share/keyrings/ros-archive-keyring.gpg
+
+###
+# Add the ROS 2 (Foxy) apt repositories to your sources list.
+###
+
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(source /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
+
+###
+# Install the Desktop Version of ROS 2 (Foxy). Contains Along with ROS, RViz, demos, tutorials
+###
+
+sudo apt update && sudo apt upgrade
+sudo apt install ros-foxy-desktop
+
+###
+# To install the Bare Bones Version of ROS 2 (Foxy), run the following commands. Contains just the communication libraries, message packages, command line tools.
+###
+
+# sudo apt install ros-foxy-ros-base
