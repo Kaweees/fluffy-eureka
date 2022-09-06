fd2e5038d0e81901ee8cced97cc0fdd17977373a
Miguel Villa Floran <49963287+Kaweees@users.noreply.github.com>
2022-09-05 21:48:47 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,14 +9,12 @@
 # Set up a locale which supports UTF-8 encoding
 ###
 
-locale  # check for UTF-8
-
 sudo apt update && sudo apt install locales
 sudo locale-gen en_US en_US.UTF-8
 sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
 export LANG=en_US.UTF-8
 
-locale  # verify settings
+locale  # verify settings by checking for UTF-8
 
 ###
 # Enable the Ubuntu Universe repository
